3c720d9861b88f689daa394540cc4cabaafc5a02
Khanh Bui <kbuiblue@gmail.com>
2023-07-30 14:10:52 +0700

    Max fib found (185th num) with u128 data type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn get_fib_sequence(nth_number: u32) {
+    let mut fib_sequence: Vec<u128> = vec![0, 1];
+
+    for i in 0..(nth_number - 1) {
+        let fib_1 = fib_sequence[i as usize];
+        let fib_2 = fib_sequence[(i + 1) as usize];
+
+        let fib_3 = fib_1 + fib_2;
+        fib_sequence.push(fib_3);
+    }
+
+    println!("{:?}", fib_sequence);
+}
+
 fn main() {
-    println!("Hello, world!");
+    get_fib_sequence(185);
 }
